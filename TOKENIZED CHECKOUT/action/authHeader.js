d9d98696f475d9c59efe527e3bda73b924bfccde
developer-sujon <developersujon@yahoo.com>
2023-03-29 05:47:53 +0600

    postman-collection-add

--- TOKENIZED CHECKOUT/action/authHeader.js
+++ TOKENIZED CHECKOUT/action/authHeader.js
@@ -2,6 +2,9 @@ const bkashConfig = require("../config/bkashConfig.json");
 const globals = require("node-global-storage");
 
 const authHeaders = async () => {
+
+console.log(globals.get("id_token"))
+
   return {
     "Content-Type": "application/json",
     Accept: "application/json",
